f2888f7171af7a1d14b2eb2e30c4a8a642380f80
Sophifc <desarrollador2@seashipping.cl>
2025-11-12 16:25:30 -0300

    Header

---
+++ frontend/src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './header.css';
+
+function Header() {
+  return (
+    <header className="selection-header">
+      <img src="/ssl-logo.png" alt="SSL Logo" />
+      <span>Página Principal</span>
+    </header>
+  );
+}
+
+export default Header;

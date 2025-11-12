2e58dc23014d77a9141dcae8f45208d23486a812
Sophifc <desarrollador2@seashipping.cl>
2025-11-12 18:23:13 -0300

    Implementación header y conexión login

--- frontend/src/ProfileSelection/ProfileSelection.jsx
+++ frontend/src/ProfileSelection/ProfileSelection.jsx
@@ -1,19 +1,28 @@
-//import React from 'react';
+import React from 'react';
 import './profileSelection.css';
+import { useNavigate } from 'react-router-dom';
+
+//COMPONENTE GLOBAL DE HEADER
+import Header from '../Header/Header' 
 
 
 //COMPONENTE SECUNDARIO PARA TARJETAS
-function ProfileCard({ title, description, features, buttonText }) {
+function ProfileCard({ title, description, features, buttonText, onCardClick }) {
   return (
     <div className="profile-card">
       <h3>{title}</h3>
+
       <p>{description}</p>
+
       <ul>
         {features.map((feature, index) => (
           <li key={index}>{feature}</li>
         ))}
       </ul>
-      <button className="access-button">{buttonText}</button>
+
+      <button className="access-button" onClick={onCardClick}>
+        {buttonText }
+      </button>
     </div>
   );
 }
@@ -21,46 +30,66 @@ function ProfileCard({ title, description, features, buttonText }) {
 
 //COMPONENTE PRINCIPAL
 function ProfileSelection() {
-  return (
-    <div className="selection-container">
-      
-      <main className="selection-main">
-        <h2>Bienvenido al Sistema SSL v4.0</h2>
-        <p>Seleccione su perfil para acceder al sistema</p>
 
-        <div className="cards-wrapper">
-          <ProfileCard 
-            title="Portal Cliente"
-            description="Acceso para empresas clientes"
-            features={[
-              "Seguimiento de solicitudes",
-              "Gestión de cotizaciones",
-              "Control de facturación"
-            ]}
-            buttonText="Acceder como Cliente"
-          />
-          <ProfileCard 
-            title="Portal Proveedor"
-            description="Acceso para proveedores"
-            features={[
-              "Recepción de cotizaciones",
-              "Gestión de órdenes",
-              "Actualización de catálogos"
-            ]}
-            buttonText="Acceder como Proveedor"
-          />
-          <ProfileCard 
-            title="Portal SSL"
-            description="Acceso administrativo"
-            features={[
-              "Gestión completa del sistema",
-              "Administración de usuarios",
-              "Reportes y análisis"
-            ]}
-            buttonText="Acceder como SSL"
-          />
-        </div>
-      </main>
+  const navigate = useNavigate();
+
+  //NAVEGACIÓN AL LOGIN
+  const handleProfileSelect = (profileType) => {
+    console.log("Perfil seleccionado:", profileType);
+    navigate('/login');
+  };
+
+
+
+  return (
+
+    <div>
+        
+        <Header />
+
+        <main className="selection-main">
+            <h2>Bienvenido al Sistema SSL v4.0</h2>
+            <p>Seleccione su perfil para acceder al sistema</p>
+
+            <div className="cards-wrapper">
+
+                <ProfileCard 
+                    title="Portal Cliente"
+                    description="Acceso para empresas clientes"
+                    features={[
+                    "Seguimiento de solicitudes",
+                    "Gestión de cotizaciones",
+                    "Control de facturación"
+                    ]}
+                    buttonText="Acceder como Cliente"
+                    onCardClick={() => handleProfileSelect('cliente')}
+                />
+
+                <ProfileCard 
+                    title="Portal Proveedor"
+                    description="Acceso para proveedores"
+                    features={[
+                    "Recepción de cotizaciones",
+                    "Gestión de órdenes",
+                    "Actualización de catálogos"
+                    ]}
+                    buttonText="Acceder como Proveedor"
+                    onCardClick={() => handleProfileSelect('Proveedor')}
+                />
+
+                <ProfileCard 
+                    title="Portal SSL"
+                    description="Acceso administrativo"
+                    features={[
+                    "Gestión completa del sistema",
+                    "Administración de usuarios",
+                    "Reportes y análisis"
+                    ]}
+                    buttonText="Acceder como SSL"
+                    onCardClick={() => handleProfileSelect('SSL')}
+                />
+            </div>
+        </main>
     </div>
   );
 }

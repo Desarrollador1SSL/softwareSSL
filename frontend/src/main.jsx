a3961e357afc1f751b8c0ee4691a21407de4a51a
Sophifc <desarrollador2@seashipping.cl>
2025-11-12 18:22:36 -0300

    Enrutamiento

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
 import './index.css'
 
+//ENRUTAMIENTO
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//RUTAS/VISTAS
 //import App from './App'
 import ProfileSelection from './ProfileSelection/ProfileSelection'
 import LoginCliente from './components/LoginCliente'
 
+const router =createBrowserRouter([
+  {
+    path: "/",
+    element: <ProfileSelection/>
+  },
+  {
+    path: "/login",
+    element: <LoginCliente/>
+  }
+]);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <App - ProfileSelection/> */}
-    <LoginCliente />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     {/* <App - ProfileSelection/> */}
+//     <LoginCliente />
+//   </StrictMode>,
+// )
